3d8e5a5605b1862dabfa69f682b98cda7443a02f
Leonardo <leon.peralta123456774@gmail.com>
2023-04-13 06:07:02 -0300

    Agregando el formulario

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-// nav-var fixed top
- // Obtén la altura de la sección de la página de aterrizaje
- var landingSectionHeight = $('.landing-section').outerHeight();
- // Agrega un evento de desplazamiento (scroll) a la ventana del navegador
- $(window).on('scroll', function() {
-   // Obtiene la posición vertical actual del desplazamiento
-   var scrollPosition = $(this).scrollTop();
-   // Comprueba si la posición vertical del desplazamiento ha pasado la altura de la sección de la página de aterrizaje
-   if (scrollPosition > landingSectionHeight) {
-     // Si ha pasado la sección de la página de aterrizaje, fija la barra de navegación
-     $('#nav-var').addClass('fixed-top');
-   } else {
-     // Si no ha pasado la sección de la página de aterrizaje, quita la fijación de la barra de navegación
-     $('#nav-var').removeClass('fixed-top');
-   }
- });
+
 
 
 
@@ -28,3 +13,45 @@
       });
     }
   });
+
+
+
+
+ // Esperar a que el DOM esté completamente cargado
+ document.addEventListener('DOMContentLoaded', function() {
+  // Obtener el formulario
+  var form = document.querySelector('.needs-validation');
+
+  // Agregar un event listener al formulario para el evento de submit
+  form.addEventListener('submit', function(event) {
+    // Verificar si el formulario es válido
+    if (!form.checkValidity()) {
+      event.preventDefault(); // Evitar el envío del formulario
+      event.stopPropagation(); // Detener la propagación del evento
+    }
+    form.classList.add('was-validated'); // Agregar la clase 'was-validated' para mostrar los mensajes de validación
+  });
+});
+
+
+
+//imagen 1 animación
+
+const miElemento = document.querySelector('.primer-img');
+
+const callback = function(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate__fadeInRight'); // Agrega la clase de animación
+      observer.unobserve(entry.target); // Deja de observar el elemento después de la animación
+    }
+  });
+};
+
+const options = {
+  threshold: 0.2 // Porcentaje de visibilidad del elemento en la ventana para activar la animación
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+observer.observe(primer-img); // Comienza a observar el elemento
